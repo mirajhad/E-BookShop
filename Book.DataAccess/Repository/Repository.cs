7d64ffc2d0bd60b66c3728c06ded97cbb8502eef
mirajhad <mirajhadish48@gmail.com>
2024-06-03 10:39:16 +0530

    Category added in product

--- Book.DataAccess/Repository/Repository.cs
+++ Book.DataAccess/Repository/Repository.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Bulky.DataAccess.Repository
 {
@@ -18,6 +19,9 @@ namespace Bulky.DataAccess.Repository
         {
             _db = db;
             this.dbSet = _db.Set<T>();
+            _db.Products
+                .Include(u => u.Category)
+                .Include(u => u.CategoryId);
         }
 
         public void Add(T entity)
@@ -25,16 +29,34 @@ namespace Bulky.DataAccess.Repository
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                             .Split(new char[] { ',' }, StringSplitOptions
+                                 .RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                             .Split(new char[] { ',' }, StringSplitOptions
+                                 .RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return dbSet.ToList();
         }
 
