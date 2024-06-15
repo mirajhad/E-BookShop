fa596fb348762b6c2e321b14267109301bb5f0fd
miraj <mirajhadish48@gmail.com>
2024-06-16 03:40:52 +0530

    role management for admin

--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -27,7 +27,27 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult RoleManagement(string userId)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(x => x.UserId == userId).RoleId;
 
+            RoleManagementVM RoleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i=> new SelectListItem
+                {
+                    Text= i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(U => U.Id == RoleID).Name;
+            return View(RoleVM);
+        }
 
         #region API CALLS
 
